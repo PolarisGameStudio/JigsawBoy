4826f0925567fcbfa26f4c11d24b57c1c5d69823
Oye <734719526@qq.com>
2018-02-21 19:17:02 +0800

    墙体生成

--- Assets/Scrpit/Init/GameStartControl.cs
+++ Assets/Scrpit/Init/GameStartControl.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class GameStartControl : MonoBehaviour
 {
-
+    //图片信息
     public JigsawResInfoBean jigsawInfoData;
+    //所有拼图信息
+    private List<JigsawBean> listJigsawBean;
 
     // Use this for initialization
     void Start()
     {
         jigsawInfoData = CommonData.selectJigsawInfo;
-        createJigsaw(jigsawInfoData);
+        initData();
     }
 
     // Update is called once per frame
@@ -26,17 +28,15 @@ public class GameStartControl : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// 初始化数据
     /// </summary>
-    /// <param name="jigsawInfoData"></param>
-    private void createJigsaw(JigsawResInfoBean jigsawInfoData)
+    private void initData()
     {
         if (jigsawInfoData == null)
             return;
         string resFilePath = jigsawInfoData.resFilePath;
         int horizontalNumber = jigsawInfoData.horizontalNumber;
         int verticalJigsawNumber = jigsawInfoData.verticalJigsawNumber;
-
         if (resFilePath == null)
         {
             LogUtil.log("没有拼图图片路径");
@@ -47,19 +47,49 @@ public class GameStartControl : MonoBehaviour
             LogUtil.log("没有拼图生成数量");
             return;
         }
-
-
         Texture2D pic2D = (Texture2D)Resources.Load(jigsawInfoData.resFilePath);
-        List<JigsawBean> listData = CreateJigsawDataUtils.createJigsawDataList(JigsawStyleEnum.Normal, horizontalNumber, verticalJigsawNumber, pic2D);
-        CreateJigsawGameObjUtil.createJigsawGameObjList(listData);
-
-        List<GameObject> containerList = CreateJigsawContainerObjUtil.createJigsawContainerObjList(listData);
-        for (int i = 0; i < listData.Count; i++)
+        if (pic2D==null)
         {
-            JigsawBean item = listData[i];
+            LogUtil.log("没有源图片");
+            return;
+        }
 
-            containerList[i].transform.position = new Vector3(item.MarkLocation.x * 3.5f, item.MarkLocation.y * 3.5f, 10);
+        createJigsaw(pic2D, horizontalNumber, verticalJigsawNumber);
+        createWall(horizontalNumber, verticalJigsawNumber);
+    }
+
+    /// <summary>
+    /// 创建拼图
+    /// </summary>
+    /// <param name="jigsawInfoData"></param>
+    private void createJigsaw(Texture2D pic2D,int horizontalNumber,int verticalJigsawNumber)
+    {
+        listJigsawBean = CreateJigsawDataUtils.createJigsawDataList(JigsawStyleEnum.Normal, horizontalNumber, verticalJigsawNumber, pic2D);
+        CreateJigsawGameObjUtil.createJigsawGameObjList(listJigsawBean);
+
+        List<GameObject> containerList = CreateJigsawContainerObjUtil.createJigsawContainerObjList(listJigsawBean);
+        for (int i = 0; i < listJigsawBean.Count; i++)
+        {
+            JigsawBean item = listJigsawBean[i];
+            containerList[i].transform.position = new Vector3(0, 0, 0);
             containerList[i].transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+    }
+
+    /// <summary>
+    /// 创建围墙
+    /// </summary>
+    /// <param name="wallWith"></param>
+    /// <param name="wallHigh"></param>
+    private void createWall(int horizontalNumber, int verticalJigsawNumber)
+    {
+        if(listJigsawBean!=null&& listJigsawBean.Count > 0)
+        {
+           JigsawBean itemJigsawBean= listJigsawBean[0];
+            float picAllWith = itemJigsawBean.JigsawWith * horizontalNumber;
+            float picAllHigh = itemJigsawBean.JigsawHigh * verticalJigsawNumber;
+            CreateGameWallUtil.createWall(picAllWith, picAllHigh);
+        }
+    
     }
 }
