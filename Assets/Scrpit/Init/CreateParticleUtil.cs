32f35669129982a80d0ca4c69bf1454eb0787e00
凸（=皿=）凸 <734719526@qq.com>
2018-03-02 15:17:25 +0800

    。。

---
+++ Assets/Scrpit/Init/CreateParticleUtil.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateParticleUtil : MonoBehaviour
+{
+
+    /// <summary>
+    /// 创建背景粒子系统
+    /// </summary>
+    /// <param name="location"></param>
+    /// <param name="particleW"></param>
+    /// <param name="particleH"></param>
+    /// <param name="particleEnum"></param>
+    public static void createBackParticle(Vector3 location, float particleW, float particleH, BackParticleEnum particleEnum)
+    {
+        ParticleSystem particleSys = null;
+        if (particleEnum.Equals(BackParticleEnum.Def))
+        {
+            particleSys = Resources.Load(ParticleResPath.Back_Def_Path) as ParticleSystem;
+
+        }
+
+        //通用参数设置
+        if (particleSys != null)
+        {
+            //设置位置
+            Transform particleSysTF = particleSys.transform;
+            particleSysTF.position = location;
+            //设置大小
+            ParticleSystem.ShapeModule shapeModule = particleSys.shape;
+            shapeModule.radius = particleW / 2f;
+
+        }
+           
+    }
+}
