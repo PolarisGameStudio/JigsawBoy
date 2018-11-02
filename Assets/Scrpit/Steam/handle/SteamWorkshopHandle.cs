34f0c5a8fa42eb8a071c2a9847627dd5c0890d39
apple <734719526@qq.com>
2018-11-02 17:42:59 +0800

    增加创意工坊相关类

---
+++ Assets/Scrpit/Steam/handle/SteamWorkshopHandle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SteamWorkshopHandle
+{
+    /// <summary>
+    /// 创建创意工坊物品
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="updateBean"></param>
+    /// <param name="callBack"></param>
+    public static void CreateWorkshopItem(BaseMonoBehaviour content, SteamWorkshopUpdateBean updateBean, ISteamWorkshopUpdateCallBack callBack)
+    {
+        if (SteamManager.Initialized)
+        {
+            SteamWorkshopUpdateImpl update = new SteamWorkshopUpdateImpl(content);
+            update.CreateWorkshopItem(updateBean, callBack);
+        }     
+    }
+}
--- Assets/Scrpit/TestScrpit.cs
+++ Assets/Scrpit/TestScrpit.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mono.Data.Sqlite;
 using Steamworks;
+using System.Threading;
 
 public class TestScrpit : BaseMonoBehaviour
 {
 
     void Start()
     {
-        LeaderBoardUtil.CreateLeaderBoardByInterval(347,358);
+        AppId_t  mAppId = new AppId_t(uint.Parse(CommonInfo.Steam_App_Id));
+        UGCQueryHandle_t handle= SteamUGC.CreateQueryUserUGCRequest(SteamUser.GetSteamID().GetAccountID(),EUserUGCList.k_EUserUGCList_Published,EUGCMatchingUGCType.k_EUGCMatchingUGCType_All,EUserUGCListSortOrder.k_EUserUGCListSortOrder_CreationOrderAsc, mAppId, mAppId,1);
+
+        CallResult<SteamUGCQueryCompleted_t> callResult = CallResult<SteamUGCQueryCompleted_t>.Create(query);
+       SteamAPICall_t c= SteamUGC.SendQueryUGCRequest(handle);
+        callResult.Set(c);
     }
 
+    public void query(SteamUGCQueryCompleted_t call ,bool bi)
+    {
+        for(uint i = 0; i < call.m_unNumResultsReturned; i++)
+        {
+            SteamUGCDetails_t details;
+            SteamUGC.GetQueryUGCResult(call.m_handle,i,out details);
+
+            LogUtil.log("item :" + i + " " + details.m_rgchTitle+ " m_hFile" + details.m_hFile+ " m_hPreviewFile" + details.m_hPreviewFile+ " m_pchFileName" + details.m_pchFileName);
+            ulong punSizeOnDisk;
+            string pchFolder;
+            uint cchFolderSize;
+            uint punTimeStamp;
+            SteamUGC.GetItemInstallInfo(details.m_nPublishedFileId,out punSizeOnDisk, out  pchFolder, details.m_nFileSize,out punTimeStamp);
+            LogUtil.log()
+        }
+    
+    }
 
 }
