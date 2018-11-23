aa638076d6caa9f94d2ad6d9682fa87e0a655637
Apple Coffee <734719526@qq.com>
2018-11-23 13:10:06 +0800

    测试

--- Assets/Scrpit/TestScrpit.cs
+++ Assets/Scrpit/TestScrpit.cs
@@ -10,23 +10,7 @@ public class TestScrpit : BaseMonoBehaviour
 
     void Start()
     {
-        SteamWorkshopHandle.QueryInstallInfo(this,1,new CallBackTest());
+      
     }
 
-    public class CallBackTest : ISteamWorkshopQueryInstallInfoCallBack
-    {
-        public void GetInstallInfoFail(SteamWorkshopQueryImpl.SteamWorkshopQueryFailEnum failEnum)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void GetInstallInfoSuccess(List<SteamWorkshopQueryInstallInfoBean> listData)
-        {
-            foreach (SteamWorkshopQueryInstallInfoBean item in listData)
-            {
-                LogUtil.log("pchFolder:" + item.pchFolder+ "punSizeOnDisk:" + item.punSizeOnDisk);
-            }
-
-        }
-    }
 }
